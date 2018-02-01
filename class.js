a8180d8f7ace7eb5b46dec909cca7edc891243ba
march-dave <ysangy@gmail.com>
2018-01-31 19:51:44 -0500

    up to date

---
+++ class.js
@@ -0,0 +1,22 @@
+
+ 
+
+class Employee extends Person {
+
+    constructor(name, age, position) {
+
+        super(name, age);
+
+        this.position = position || 'unknown position';
+
+    }
+
+ 
+
+    getPosition() {
+
+        return this.position;
+
+    }
+
+}
---
+++ prototype.js
@@ -0,0 +1,93 @@
+var extendClass = (function() {
+
+    function Bridage() {}
+
+    return function(Parent, Child) {
+
+        Bridge.prototype = Parent.prototype;
+
+        Child.prototype = new Bridge();
+
+        Child.prototype.constructor = Child;
+
+        Child.prototype.supreClass = Parent;
+
+    }
+
+})();
+
+ 
+
+function Person(name, age) {
+
+    this.name = name || 'unknown name';
+
+    this.age = age || 'unknown age';
+
+}
+
+ 
+
+Person.prototype.getName = function() {
+
+    return this.name;
+
+}
+
+ 
+
+Person.prototype.getAge = function() {
+
+    return this.age;
+
+}
+
+ 
+
+function Employee(name, age, position) {
+
+    this.superClass(name, age);
+
+    this.position = position || 'unknown position';
+
+}
+
+ 
+
+extendClass(Person, Employee);
+
+Employee.prototype.getPosition = function() {
+
+    return this.position;
+
+}
+
+ 
+
+class Person {
+
+    constructor(name, age) {
+
+        this.name = name || 'unknow name';
+
+        this.age = age || 'unknow age';
+
+    }
+
+ 
+
+    getName() {
+
+        return this.name;
+
+    }
+
+ 
+
+    getAge() {
+
+        return this.age;
+
+    }
+
+}
