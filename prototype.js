79f124de086903f266c05fbac9123cd1c49edc5a
march-dave <ysangy@gmail.com>
2018-02-01 20:35:16 -0500

    up to date

--- prototype.js
+++ prototype.js
@@ -13,53 +13,37 @@ var extendClass = (function() {
         Child.prototype.supreClass = Parent;
 
     }
-
 })();
-
  
 
 function Person(name, age) {
-
     this.name = name || 'unknown name';
-
     this.age = age || 'unknown age';
-
 }
 
  
 
 Person.prototype.getName = function() {
-
     return this.name;
-
 }
 
  
 
 Person.prototype.getAge = function() {
-
     return this.age;
-
 }
 
- 
 
 function Employee(name, age, position) {
-
     this.superClass(name, age);
-
     this.position = position || 'unknown position';
-
 }
-
  
 
 extendClass(Person, Employee);
 
 Employee.prototype.getPosition = function() {
-
     return this.position;
-
 }
 
  
@@ -74,20 +58,19 @@ class Person {
 
     }
 
- 
-
     getName() {
-
         return this.name;
-
     }
-
- 
 
     getAge() {
-
         return this.age;
-
     }
+}
 
+
+class human {
+
+    constructor(name, age, gender) {
+        
+    }
 }
